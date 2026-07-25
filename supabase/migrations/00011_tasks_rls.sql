3006b55ac2d280a8d8986746a2590d112b9e3ace
Weslley Valente <weslleyvalente@msn.com>
2026-07-24 23:24:18 -0300

    feat: add RLS policies for tasks and task_events

---
+++ supabase/migrations/00011_tasks_rls.sql
@@ -0,0 +1,26 @@
+ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
+ALTER TABLE task_events ENABLE ROW LEVEL SECURITY;
+
+DO $$
+DECLARE
+  tbl text;
+BEGIN
+  FOREACH tbl IN ARRAY ARRAY['tasks', 'task_events'] LOOP
+    EXECUTE format(
+      'CREATE POLICY "%1$s_select" ON %1$s FOR SELECT USING (organization_id IN (SELECT get_user_org_ids()))',
+      tbl
+    );
+    EXECUTE format(
+      'CREATE POLICY "%1$s_insert" ON %1$s FOR INSERT WITH CHECK (organization_id IN (SELECT get_user_org_ids()))',
+      tbl
+    );
+    EXECUTE format(
+      'CREATE POLICY "%1$s_update" ON %1$s FOR UPDATE USING (organization_id IN (SELECT get_user_org_ids()))',
+      tbl
+    );
+    EXECUTE format(
+      'CREATE POLICY "%1$s_delete" ON %1$s FOR DELETE USING (organization_id IN (SELECT get_user_org_ids()))',
+      tbl
+    );
+  END LOOP;
+END $$;
